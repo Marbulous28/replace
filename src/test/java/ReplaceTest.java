c7b267c8efd680508a224749f207ee590f00db5a
Peter Beach <peterbeach2@gmail.com>
2016-04-22 09:59:38 -0700

    all vowels are now being replaced. Moving on to front end.

--- src/test/java/ReplaceTest.java
+++ src/test/java/ReplaceTest.java
@@ -10,16 +10,31 @@ public class ReplaceTest {
     String expected = "c-r";
     assertEquals(expected, testReplace.replaceVowels("car"));
   }
+
   @Test
   public void replaceVowels_replaceAE_String() {
     Replace testReplace = new Replace();
     String expected = "h-y th-r- w-nd-r-r";
     assertEquals(expected, testReplace.replaceVowels("hey there wanderer"));
   }
-  // @Test
-  // public void replaceVowels_replaceA_String() {
-  //   Replace testReplace = new Replace();
-  //   String expected = "h-llo there";
-  //   assertEquals(expected, testReplace.replaceVowels("hallo there"));
-  // }
+
+  @Test
+  public void replaceVowels_replaceAEO_String() {
+    Replace testReplace = new Replace();
+    String expected = "h-y th-r- w-nd-r-r, g- d-nc-";
+    assertEquals(expected, testReplace.replaceVowels("hey there wanderer, go dance"));
+  }
+
+  @Test
+  public void replaceVowels_replaceAEIO_String() {
+    Replace testReplace = new Replace();
+    String expected = "h-y th-r- w-nd-r-r, g- d-nc- -t -ff";
+    assertEquals(expected, testReplace.replaceVowels("hey there wanderer, go dance it off"));
+  }
+  @Test
+  public void replaceVowels_replaceAEIOU_String() {
+    Replace testReplace = new Replace();
+    String expected = "h-y th-r- w-nd-r-r, g- d-nc- -t --t";
+    assertEquals(expected, testReplace.replaceVowels("hey there wanderer, go dance it out"));
+  }
 }
